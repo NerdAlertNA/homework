71af7b51bca60a1177678a220fa21ae9bdd2ad15
jacob <jacob.butler1994@gmail.com>
2017-03-14 10:24:01 -0700

    completed GET routes

--- week04/day02/pizza-express/app.js
+++ week04/day02/pizza-express/app.js
@@ -1,8 +1,21 @@
 // app.js
 
 var express = require('express'); //require express package
-
 var app = express(); //save an express module as 'app'
+
+
+app.get('/', function(req,res) {
+	res.send('Welcome to Pizza Express!');
+});
+
+app.get('/topping/:type', function(req, res, next){
+	res.send(`${req.params.type} pizza! Good choice`);
+});
+
+app.get('/order/:amount/:size', function(req, res, next){
+	res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+});
+
 
 var port = 3000; //assigns this server to port 3000
 
