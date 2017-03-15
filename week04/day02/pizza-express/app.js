de6575b549f01aac01e7280fd62a1a4ef4ae4aef
jacob <jacob.butler1994@gmail.com>
2017-03-14 18:45:45 -0700

    refactored routes and added views

--- week04/day02/pizza-express/app.js
+++ week04/day02/pizza-express/app.js
@@ -2,18 +2,38 @@
 
 var express = require('express'); //require express package
 var app = express(); //save an express module as 'app'
+var hbs = require('hbs');
+app.set("view engine", "hbs");
+// app.set('views', './views');
+
+
+// app.get("/test/:someValue", function(req, res, next){
+//   res.render("index.hbs", {
+//     message: req.params.someValue
+//   });
+// });
+
 
 
 app.get('/', function(req,res) {
-	res.send('Welcome to Pizza Express!');
+	res.render('index.hbs', {
+		message: "Welcome to Pizza Express!"  // doesnt work yet
+	});
+	// res.send('Welcome to Pizza Express!');
 });
 
 app.get('/topping/:type', function(req, res, next){
-	res.send(`${req.params.type} pizza! Good choice`);
+	// res.send(`${req.params.type} pizza! Good choice`);
+	res.render('toppings.hbs', {
+		message: `${req.params.type} pizza! Good choice.`
+	});
 });
 
 app.get('/order/:amount/:size', function(req, res, next){
-	res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+	res.render('order', {
+		message: `your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute`
+	});
+	// res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
 });
 
 
